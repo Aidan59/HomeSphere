01db65c97a9e5da71ebb1ca99e0bdd159197c850
Danil <dplotnikov987@gmail.com>
2025-02-09 12:08:02 +0300

    Project initialization

---
+++ src/main/java/com/example/HomeSphere/HomeSphereApplication.java
@@ -0,0 +1,13 @@
+package com.example.HomeSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HomeSphere/HomeSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HomeSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
