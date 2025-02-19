7c32a532d006e26cfbd1e7952334efedb3aebaf7
Danil <dplotnikov987@gmail.com>
2025-02-19 16:47:00 +0300

    Login and Register pages added

--- src/main/java/com/example/HomeSphere/config/SecurityConfig.java
+++ src/main/java/com/example/HomeSphere/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.example.HomeSphere.config;
 
 import com.example.HomeSphere.security.AuthProviderImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.security.AuthProvider;
 
@@ -19,5 +23,10 @@ public class SecurityConfig  extends WebSecurityConfiguration {
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authProvider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
